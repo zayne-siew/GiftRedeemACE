e6ba6255a4e35832e500c759e0bfe830372102e8
zayne-siew <zayne.siew@gmail.com>
2024-02-18 18:36:49 +0800

    testing formatDate with UTC

--- server/tests/format_date.test.ts
+++ server/tests/format_date.test.ts
@@ -2,7 +2,7 @@ import { formatDate } from '../app';
 
 describe('Testing formatDate()', () => {
     test('Format epoch 0', () => {
-        const dateString: string = '01/01/1970 07:30:00';
+        const dateString: string = '01/01/1970 00:00:00';
         const date: Date = new Date(dateString);
 
         expect(formatDate(new Date(0))).toEqual(dateString);
@@ -10,7 +10,7 @@ describe('Testing formatDate()', () => {
     });
     test('Format epoch 1708267593000', () => {
         const epoch: number = 1708267593000;
-        const dateString: string = '18/02/2024 22:46:33';
+        const dateString: string = '18/02/2024 14:46:33';
 
         expect(formatDate(new Date(epoch))).toEqual(dateString);
     })
